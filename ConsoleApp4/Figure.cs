92eb3574bc6db5536e0f8c76f308ec1239a4801e
stranger4444 <dronovdenis36@gmail.com>
2023-10-02 10:09:34 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Figure.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Figure
+    {
+        private Point[] points;
+
+        public Figure(Point[] points)
+        {
+            if (points.Length < 3 || points.Length > 5)
+            {
+                throw new ArgumentException("The number of points should be from 3 to 5");
+            }
+
+            this.points = points;
+        }
+
+        public double GetSideLength(Point A, Point B)
+        {
+            double deltaX = B.X - A.X;
+            double deltaY = B.Y - A.Y;
+            return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+        }
+
+        public void CalculatePerimeter()
+        {
+            double perimeter = 0;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                int nextIndex = (i + 1) % points.Length;
+                double sideLength = GetSideLength(points[i], points[nextIndex]);
+                perimeter += sideLength;
+            }
+
+            Console.WriteLine($"Perimeter of polygon: {perimeter}");
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point[] points = new Point[]
+        {
+            new Point(5, 0, "A"),
+            new Point(0, 4, "B"),
+            new Point(4, 0, "C"),
+            new Point(0, 5, "D"),
+        };
+
+        Figure figure = new Figure(points);
+        figure.CalculatePerimeter();
+
+        Console.ReadLine();
+    }
+}
